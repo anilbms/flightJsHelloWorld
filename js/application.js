3a694c0bce7b08644012f935e231606f06f26896
stefanRitter <stefan@stefanritter.com>
2013-10-27 14:29:49 +0000

    add basic component

--- js/application.js
+++ js/application.js
@@ -1,2 +1,13 @@
-'use strict';
 
+jQuery(function($) {
+  'use strict';
+
+  var helloWorld = flight.component(function() {
+
+    console.log('my frist component constructor');
+    
+  });
+
+  helloWorld.attachTo(document);
+
+});
